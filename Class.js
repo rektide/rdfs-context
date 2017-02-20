debeedeb6fdfea18d0685384e17ae840ef9f7443
rektide de la faye <rektide@voodoowarez.com>
2017-02-19 21:18:07 -0500

    Build out Class.js.

---
+++ Class.js
@@ -0,0 +1,10 @@
+module.exports= {
+	"@id": "http://www.w3.org/2000/01/rdf-schema#Class",
+	"@type": "http://www.w3.org/2000/01/rdf-schema#Class",
+	"@context": {
+		"Class": {
+			"@id": "http://www.w3.org/2000/01/rdf-schema#Class"
+			"@type": "@id"
+		}
+	}
+}
