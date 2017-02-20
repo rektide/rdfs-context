aacbd844418227f0527bd080fcc8fadb571fe4c5
rektide de la faye <rektide@voodoowarez.com>
2017-02-19 20:59:45 -0500

    Initial commit

---
+++ owl-jsonld.js
@@ -0,0 +1,21 @@
+module.exports= {
+	"@id": "http://www.w3.org/2000/01/rdf-schema#",
+	"@context": {
+		"rdfs": "http://www.w3.org/2000/01/rdf-schema#"
+		"http://www.w3.org/2000/01/rdf-schema#Class": {
+			
+		},
+		"http://www.w3.org/2000/01/rdf-schema#Property": {
+			
+		},
+		"http://www.w3.org/2000/01/rdf-schema#subPropertyOf": {
+			
+		},
+		"http://www.w3.org/2000/01/rdf-schema#subClassOf": {
+			
+		},
+		"http://www.w3.org/2000/01/rdf-schema#type": {
+			"@type": "http://www.w3.org/2000/01/rdf-schema#Class"
+		}
+	}
+}
