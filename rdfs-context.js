db6387801de016933b6cde74b7b28df0ae7065e1
rektide de la faye <rektide@voodoowarez.com>
2017-02-20 00:36:17 -0500

    Replace rdfs-context.js with auto-generated.

--- rdfs-context.js
+++ rdfs-context.js
@@ -1,24 +1,10 @@
-var atId= require( "./@id")
-module.exports= {
-	"@id": atId,
-	"@type": "http://json-ld.org/#context",
-	"@context": {
-		"rdfs": atId,
-		[atId+ "Resource"]: require( "./Resource"),
-		[atId+ "Literal"]: require( "./Literal"),
-		[atId+ "Class"]: require( "./Class"),
-		[atId+ "Datatype"]: require( "./Datatype"),
-		[atId+ "Container"]: require( "./Container"),
-		[atId+ "ContainerMembershipProperty"]: require( "./ContainerMembershipProperty"),
+#!/usr/bin/env node
+"use strict"
 
-		[atId+ "subClassOf"]: require( "./subClassOf"),
-		[atId+ "subPropertyOf"]: require( "./subPropertyOf"),
-		[atId+ "domain"]: require( "./domain"),
-		[atId+ "range"]: require( "./range"),
-		[atId+ "label"]: require( "./label"),
-		[atId+ "comment"]: require( "./comment"),
-		[atId+ "member"]: require( "./member"),
-		[atId+ "seeAlso"]: require( "./seeAlso"),
-		[atId+ "isDefinedBy"]: require( "./isDefinedBy")
-	}
+module.exports= {
+	"@context": require( "./prefixes.json"),
+	"@graph": require( "./rdfs-context.json")
+}
+if( typeof(require)!== "undefined"&& require.main=== module){
+	console.log( JSON.stringify( module.exports, null, "\t"))
 }
